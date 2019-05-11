b5449fa0f5f77e788d603ae49c473ec7a3c7f6d4
Miguel Machado <miiguellmp@gmail.com>
2019-05-11 17:05:20 -0300

    conditionally rendering content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { Component } from 'react';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends Component {
+  // very first function that is going to be called any time an instance
+  // of this call is created
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      lat: null,
+      errorMessage: '',
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude } = position.coords;
+        this.setState({ lat: latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      },
+    );
+  }
+
+  render() {
+    const { lat, errorMessage } = this.state;
+    if (errorMessage && !lat) {
+      return <div>Error: {errorMessage}</div>;
+    }
+
+    if (!errorMessage && lat) {
+      return <div>Latitude: {lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
 export default App;
